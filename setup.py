22481aaf7451f0a1d17d446a091f8166c7a69568
Humaira Halim <75388609+Humairabh@users.noreply.github.com>
2022-07-11 12:28:23 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='package',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/Humairabh/Booklover_14',
+      author='Humaira Halim',
+      author_email='hbh4bv@virginia.com',
+      license='MIT',
+      packages=['demo'])
